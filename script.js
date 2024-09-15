2a65a4106e03b202681aeb4bee58efa3a759960e
Felipe dos Santos Bento <dsbfelipe@outlook.com>
2024-09-15 15:46:18 -0300

    Create the logic to play a round

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 const options = ["rock", "paper", "scissors"];
+let humanScore = 0;
+let computerScore = 0;
 
 function getComputerChoice() {
   const randomIndex = Math.floor(Math.random() * 3);
@@ -10,4 +12,27 @@ function getHumanChoice() {
   return humanChoice.toLowerCase();
 }
 
-console.log(getHumanChoice())
+function playRound(humanChoice, computerChoice) {
+  if  (
+      (humanChoice === "rock" || computerChoice === "paper") &&
+      (humanChoice === "paper" || computerChoice === "scissors") &&
+      (humanChoice === "scissors" || computerChoice === "rock")
+      ) {
+        console.log(`You lose! ${computerChoice} beats ${humanChoice}`)
+        computerScore++;
+      }
+
+  if  (
+      (humanChoice === "paper" || computerChoice === "rock") &&
+      (humanChoice === "scissors" || computerChoice === "paper") &&
+      (humanChoice === "rock" || computerChoice === "scissors")
+      ) {
+        console.log(`You win! ${humanChoice} beats ${computerChoice}`)
+        humanScore++;
+      }
+
+  if (humanChoice === computerChoice) {
+    console.log(`There's a draw! you both chose ${humanChoice}`)
+  }
+}
+
