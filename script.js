33ce3e0eb79f3b30536bfbbe492b5fdb83bcd622
Felipe dos Santos Bento <dsbfelipe@outlook.com>
2024-09-15 15:31:42 -0300

    Create the logic to get the player choice

--- script.js
+++ script.js
@@ -4,3 +4,10 @@ function getComputerChoice() {
   const randomIndex = Math.floor(Math.random() * 3);
   return options[randomIndex];
 }
+
+function getHumanChoice() {
+  const humanChoice = window.prompt("Insert your choice");
+  return humanChoice.toLowerCase();
+}
+
+console.log(getHumanChoice())
